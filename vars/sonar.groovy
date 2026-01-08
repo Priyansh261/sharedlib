3ede44fca6964fc022f8a8a8af0cf974a3210b6b
Priyanshu Negi <113706789+Priyansh261@users.noreply.github.com>
2026-01-08 22:36:59 +0530

    Create sonar.groovy

---
+++ vars/sonar.groovy
@@ -0,0 +1,17 @@
+def call(String projectName, String projectKey) {
+    stage('Code Quality - SonarQube') {
+        def scannerHome = tool 'sonar-scanner'
+        withSonarQubeEnv('sonar-scanner') {
+            sh """
+            export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+            export PATH=\$JAVA_HOME/bin:\$PATH
+            ${scannerHome}/bin/sonar-scanner \
+              -Dsonar.projectName=${projectName} \
+              -Dsonar.projectKey=${projectKey} \
+              -Dsonar.sources=src \
+              -Dsonar.exclusions=**/target/** \
+              -Dsonar.java.binaries=target/classes
+            """
+        }
+    }
+}
